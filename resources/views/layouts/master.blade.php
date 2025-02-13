27ba706b75874d22b74f1f0b1a4249968d5aab6b
chaiwatt <chaiwat_thaweechan@yahoo.com>
2025-02-13 16:27:13 +0700

    update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -40,18 +40,7 @@
 
     <!--====== Dynamic theme changing =====-->
 
-    <script>
-        var baseUrl = $('meta[name="base-url"]').attr('content') + '/';
-    
-        $.ajaxSetup({
-            beforeSend: function(xhr, settings) {
-                // ป้องกัน baseUrl ซ้ำซ้อน
-                if (!settings.url.startsWith('http') && !settings.url.startsWith(baseUrl)) {
-                    settings.url = baseUrl + settings.url;
-                }
-            }
-        });
-    </script>
+
 
     <?php
     $theme_name = 'default';
@@ -256,6 +245,21 @@
     <!-- ====== Loading ====== -->
     <script src="https://cdn.jsdelivr.net/npm/gasparesganga-jquery-loading-overlay@2.1.7/dist/loadingoverlay.min.js"></script>
     <script src="{{asset('plugins/components/sweet-alert2/sweetalert2.all.min.js')}}"></script>
+
+
+    <script>
+        var baseUrl = $('meta[name="base-url"]').attr('content') + '/';
+    
+        $.ajaxSetup({
+            beforeSend: function(xhr, settings) {
+                // ป้องกัน baseUrl ซ้ำซ้อน
+                if (!settings.url.startsWith('http') && !settings.url.startsWith(baseUrl)) {
+                    settings.url = baseUrl + settings.url;
+                }
+            }
+        });
+    </script>
+
     <script type="text/javascript">
         $(document).ready(function() {
 
